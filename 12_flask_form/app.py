fdc145d279e75e398b3a99b0c982e87ac98c87d2
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-09-10 11:47:48 +0100

    forms em flash e html

---
+++ 12_flask_form/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+class Aluno:
+    def __init__(self, nome, turma):
+        self.nome = nome
+        self.turma = turma
+
+
+alunos = [
+    Aluno('Carlos', 'py2024'),
+    Aluno('Ana', 'py2024'),
+    Aluno('Rita', 'C++2024'),
+    Aluno('Maria', 'py2022'),
+    Aluno('Luis', 'py2022')
+]
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("add_form.html")
+
+@app.route('/txt_aleatorio', methods=['POST'])
+def add_aluno():
+
+    nome = request.form['nome_aluno']
+    turma = request.form['turma_aluno']
+    al = Aluno(nome, turma)
+    alunos.append(al)
+
+    return redirect(url_for("lista_alunos"))
+
+
+
+@app.route('/alunos')
+def lista_alunos():
+    return render_template("lista_alunos.html",
+                           al=alunos)
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Get  - Ler dados
+Post - Escrever dados
+
+"""
+
