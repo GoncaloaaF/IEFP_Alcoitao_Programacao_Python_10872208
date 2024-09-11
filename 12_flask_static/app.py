123ef5c529123ff641db8d7184c72b8e0fa594fa
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-09-11 10:20:50 +0100

    seletores css

--- 12_flask_static/app.py
+++ 12_flask_static/app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect
-
+import uuid
 
 app = Flask(__name__)
+
+class Contacto:
+    def __init__(self, nome, telefone):
+        self.id = uuid.uuid4()
+        self.nome = nome
+        self.telefone = telefone
+
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("homepage.html")
+
+    ct = Contacto("nome", 12345)
+
+    return render_template("homepage.html", ct=ct)
 
 
 if __name__ == '__main__':
